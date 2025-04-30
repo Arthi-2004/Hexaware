f09e91a422671a0e286bcc07d52f673c9f8b7959
Arthi <107358133+Arthi-2004@users.noreply.github.com>
2025-04-30 10:44:26 +0530

    Delete CaseStudy directory

--- CaseStudy/entity/product.py
+++
@@ -1,7 +0,0 @@
-class Product:
-    def __init__(self, product_id, name, price, description, stockQuantity):
-        self.product_id = product_id
-        self.name = name
-        self.price = price
-        self.description = description
-        self.stockQuantity = stockQuantity  # Match the column name from the database
