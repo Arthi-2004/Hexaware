d800c38e7d8cfbf020c12241e0230ece0f3594e3
Arthi <107358133+Arthi-2004@users.noreply.github.com>
2025-04-30 09:26:56 +0530

    Delete EcommerceProject directory

--- Casestudy/EcommerceProject/exception/not_found_exception.py
+++
@@ -1,8 +0,0 @@
-class NotFoundException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
-
-    def __str__(self):
-        return f"Not Found: {self.message}"
-
